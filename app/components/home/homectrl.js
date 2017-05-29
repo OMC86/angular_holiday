5bc63dbc361d1f80ab05ae223dbefe92a066daf2
Chris <christoal@outlook.com>
2017-05-29 17:13:23 +0800

    added tabs logic

--- app/components/home/homectrl.js
+++ app/components/home/homectrl.js
@@ -1,6 +1,19 @@
 angular.module('EmeraldApp')
 
-	.controller('HomeCtrl', ['$scope', function($scope){
-		$scope.title = 'home page'
+	.controller('HomeCtrl', ['$scope', '$location', function($scope, $location){
+		
+		$scope.tab = 1;
 
-	}])
+	    $scope.setTab = function(newTab){
+	      $scope.tab = newTab;
+	    };
+
+	    $scope.isSet = function(tabNum){
+	      return $scope.tab === tabNum;
+	    };
+
+	    $scope.goTo= function(id){
+	    	$location.url(id);
+	    };
+		
+  	}]);
