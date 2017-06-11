de34bc9a9f38fa8dc55091abf7cbcb6013529d18
Chris <christoal@outlook.com>
2017-06-11 14:34:44 +0800

    added show hide functionality to forms

--- app/components/booking/bookingCtrl.js
+++ app/components/booking/bookingCtrl.js
@@ -2,6 +2,15 @@ angular.module('EmeraldApp')
 
 	.controller('BookingCtrl', ['$scope', function($scope) {
 		
-		
+		$scope.visible = false;
+		$scope.visibleCalc = false;
+
+		$scope.showForm = function(){
+			$scope.visible = $scope.visible ? false : true; 
+		};
+
+		$scope.showCalc = function(){
+			$scope.visibleCalc = $scope.visibleCalc ? false : true; 
+		};
 		
 	}]);
