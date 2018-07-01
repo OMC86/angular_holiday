76a7e955b30c5e84d723ecc0433192a5b6c0c69e
Chris <ctoal@bink.com>
2018-07-01 20:40:06 +0100

    added terms, services and updated bookings to reflect required changes

--- app/app.js
+++ app/app.js
@@ -4,39 +4,47 @@ angular.module('EmeraldApp', [
 	'ngAnimate',
 	])
 
-.config(function($locationProvider, $routeProvider){
-	$locationProvider.html5Mode(true);  // Enable href routing without hashes
+.config(function($locationProvider, $routeProvider) {
+    $locationProvider.html5Mode(true);  // Enable href routing without hashes
 
-	$routeProvider
+    $routeProvider
 
-	.when('/', {
-        templateUrl: 'app/components/home/home.html',
-	    controller: 'HomeCtrl'
-	})
+        .when('/', {
+            templateUrl: 'app/components/home/home.html',
+            controller: 'HomeCtrl'
+        })
 
-	.when('/general', {
-		templateUrl: 'app/components/generalInfo/generalInfo.html',
-		controller: 'GeneralInfoCtrl'
-	})
+        .when('/general', {
+            templateUrl: 'app/components/generalInfo/generalInfo.html',
+            controller: 'GeneralInfoCtrl'
+        })
 
-	.when('/activities', {
-		templateUrl: 'app/components/activities/activities.html',
-		controller: 'ActivitiesCtrl'
-	})
+        .when('/activities', {
+            templateUrl: 'app/components/activities/activities.html',
+            controller: 'ActivitiesCtrl'
+        })
 
-	.when('/inclusive', {
-		templateUrl: 'app/components/inclusive/inclusive.html',
-		controller: 'InclusiveCtrl'
-	})
+        .when('/inclusive', {
+            templateUrl: 'app/components/inclusive/inclusive.html',
+            controller: 'InclusiveCtrl'
+        })
 
-	.when('/about', {
-		templateUrl: 'app/components/about/about.html',
-		controller: 'AboutCtrl'
-	})
+        .when('/about', {
+            templateUrl: 'app/components/about/about.html',
+            controller: 'AboutCtrl'
+        })
 
-	.when('/booking', {
-		templateUrl: 'app/components/booking/booking.html',
-		controller: 'BookingCtrl'
-	});
+        .when('/booking', {
+            templateUrl: 'app/components/booking/booking.html',
+            controller: 'BookingCtrl'
+        })
+
+		.when('/services', {
+			templateUrl: 'app/components/services/services.html'
+		})
+
+		.when('/terms', {
+			templateUrl: 'app/components/terms/terms.html'
+		});
 
 });
